b97c45e30fd996c862637ee61ee82d60ea948bf5
Greg Pinero <gregpinero@gmail.com>
2012-08-29 09:10:10 -0400

    Updating instructions

--- example/get_sample_data.sh
+++ example/get_sample_data.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # Download sample data for MSG
-#wget "https://github.com/tinathu/msg/raw/master/example/msg.cfg" --no-check-certificate -O msg.cfg
-#wget "https://github.com/tinathu/msg/raw/master/example/barcodes_file" --no-check-certificate -O barcodes_file.txt
 
 FILES="ftp://anonymous@ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX029/SRX029935/SRR071201/SRR071201.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX032/SRX032362/SRR074287/SRR074287.sra ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX032/SRX032363/SRR074288/SRR074288.sra ftp://anonymous@ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX029/SRX029935/SRR071201/SRR071201.sra"
 
